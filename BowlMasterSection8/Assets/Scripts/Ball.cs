b9ceff9e51a810c6896324a9fd09d19622fb8453
SpeakingCraniums <wesley.weissenberger@gmail.com>
2017-03-04 19:53:36 -0500

    4thMarchWork
    Ball, pins, rolling and camera follow implemented.

---
+++ BowlMasterSection8/Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	private Rigidbody ballBody;
+	private AudioSource ballRollSound;
+	//private bool soundShouldPlay = true;
+
+	public float launchSpeed;
+
+	// Use this for initialization
+	void Start () {
+		ballBody = GetComponent<Rigidbody>();
+		ballRollSound = GetComponent<AudioSource>();
+		ballBody.velocity = new Vector3(0,0,launchSpeed);
+		ballRollSound.Play();
+	}
+}
